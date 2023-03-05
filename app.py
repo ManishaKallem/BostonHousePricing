6cfb6e3eb7b5a6ab647c3b62e42745bb0c9dcdfa
Manisha Kallem <manishareddy149@gmail.com>
2023-03-06 00:37:53 +0530

    Prediction API

--- app.py
+++ app.py
@@ -18,5 +18,12 @@ def predict_api():
     output=regmodel.predict(new_data)
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    new_data=scalar.transform((np.array(data)).reshape(1,-1))
+    output=regmodel.predict(new_data)[0]
+    return render_template("home.html",prediction_text="Predicted House Price is {}".format(output))
+
 if __name__ == "__main__":
     app.run(debug = True)
